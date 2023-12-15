8aca91e220451dd7785e9d45e14ce0f1a00f0717
hmorrow <howard.morrow@gmail.com>
2023-12-15 08:40:48 -1000

    Added various hello world example docs

---
+++ hello.cs
@@ -0,0 +1,9 @@
+namespace MyNameSpace;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ hello.java
@@ -0,0 +1,7 @@
+// Your First Java Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
